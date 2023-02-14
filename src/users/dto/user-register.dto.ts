4252e3595bed8019474a9428cfb625ddc1adc576
Maria Chuikina <4uikina@gmail.com>
2023-02-14 12:59:10 +0100

    middleware validate class validator transformer

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from 'class-validator/types/decorator/decorators';
+
 export class UserRegisterDto {
+	@IsEmail({}, { message: 'Incorrect email' })
 	email: string;
+
+	@IsString({ message: 'no password provided' })
 	password: string;
+
+	@IsString({ message: 'no name provided' })
 	name: string;
 }
